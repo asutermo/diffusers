b0cf439a95fdea6dc28caa2f077e55171ce7ec0a
andrew <andrew@forming.ai>
2024-06-07 14:52:42 -0600

    cmon

--- examples/controlnet/accelerate_controlnet_sagemaker.py
+++ examples/controlnet/accelerate_controlnet_sagemaker.py
@@ -2,17 +2,12 @@ import os
 import subprocess
 import sys
 
-def main(args):
-    try:
-        #a_args.extend(args)
-        print(a_args)
-        result = subprocess.run(a_args, capture_output=True, text=True, check=True)
-        print("Output from other_script.py:\n", result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error running other_script.py with accelerate:\n", e.stderr)
-
 if __name__ == "__main__":
     current_script_directory = os.path.dirname(os.path.abspath(__file__))
+    with os.scandir(current_script_directory) as entries:
+        for entry in entries:
+            if entry.is_file():
+                print(entry.name)
     a_args = ["accelerate", "launch", "--num_processes=8", "--multi_gpu", f"{current_script_directory}/train_controlnet_sdxl.py"] +  sys.argv[1:]
     try:
         #a_args.extend(args)
